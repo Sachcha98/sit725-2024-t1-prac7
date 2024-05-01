1d7435e97aacc3e6d79c9ee738942bc085d33fdf
sachin jayakody <Sachi.jayakody98@gmail.com>
2024-05-01 17:01:13 +1000

    DB Connection update

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://root:root@cluster1.2ockrve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const uri = "mongodb+srv://sachijayakody98:mongoDB735@cluster0.pyujbeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri, {
     serverApi: {
